e668e8c1735bcc89c642f4c04f9806666da5d4a8
Dan <Daniel@alberg-long.co.uk>
2024-05-17 00:21:34 +1000

    Updated benchmark page with only one page and dropdown to select dataset and/or approach; Automated benchmark analysis

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 // script.js
 // fetch data from file
+
+var selectedDataset = "MNIST";
+var datasetOptions = [];
+
+const datasetSelect = document.getElementById('dataset-select');
+datasetSelect.addEventListener('change', (event) => {
+  selectedDataset = event.target.value;
+  populateDatasetDescription();
+  populateDatasetTable();
+});
+
+var selectedApproach = "All";
+var approachOptions = [
+  "All",
+  "Supervised Learning",
+  "Unsupervised Learning",
+  "Reinforcement Learning",
+  "Hybrid Approaches",
+  "Spike-Based Backpropagation Variants",
+  "Reservoir Computing and Liquid State Machines",
+];
+const approachSelect = document.getElementById('approach-select');
+approachOptions.forEach((dataset) => {
+  const option = document.createElement('option');
+  option.value = dataset;
+  option.textContent = dataset;
+  approachSelect.appendChild(option);
+});
+approachSelect.addEventListener('change', (event) => {
+  selectedApproach = event.target.value;
+  populateDatasetTable();
+});
+
+
 const fetchData = async (filePath) => {
   try {
     const response = await fetch(filePath);
@@ -11,20 +45,58 @@ const fetchData = async (filePath) => {
   }
 };
 
-const populateTable = async () => {
-  const dataName = document.currentScript.getAttribute('data-name');
-  const filePath = `../data/${dataName}.json`;
-  var data = await fetchData(filePath);
+const getDatasetOptions = async () => {
+  var datasets = await fetchData("./data/datasets.json");
+
+  datasetOptions = [];
+  datasets.forEach((dataset) => {
+    datasetOptions.push(dataset.name);
+  });
+
+  const datasetSelect = document.getElementById('dataset-select');
+
+  datasetOptions.forEach((dataset) => {
+    const option = document.createElement('option');
+    option.value = dataset;
+    option.textContent = dataset;
+    datasetSelect.appendChild(option);
+  });
+}
+
+const populateDatasetTable = async () => {
+  var papers = await fetchData("./data/papers.json");
+
+  console.log(papers);
+
+  // Filter data by approach dataset
+  var approaches = [];
+  papers.forEach((paper) => {
+    paper.approaches.forEach((approach) => {
+      if (selectedApproach == "All" || approach.category === selectedApproach) {
+        if (approach.dataset === selectedDataset) {
+          approach["title"] = paper.title;
+          approach["authors"] = paper.authors;
+          approach["last_revised_date"] = paper.last_revised_date;
+          approach["paper"] = paper.paper;
+          
+          approaches.push(approach);
+        }
+      }
+    });
+  });
 
   // sort data by accuracy
-  data.sort((a, b) => {
+  approaches.sort((a, b) => {
     return b["accuracy"] - a["accuracy"];
   });
+
+  console.log(approaches);
   
   const table = document.querySelector('table');
   const tableBody = table.querySelector('tbody');
+  tableBody.innerHTML = '';
 
-  data.forEach((row) => {
+  approaches.forEach((row) => {
     const tableRow = document.createElement('tr');
 
     var tableCell = document.createElement('td');
@@ -63,6 +135,32 @@ const populateTable = async () => {
   
     tableBody.appendChild(tableRow);
   });
+
+  const datasetDescription = document.getElementById(`dataset-count`);
+  datasetDescription.textContent = `Benchmarked: ${approaches.length}`;
 };
 
-populateTable();
+const populateDatasetDescription = async () => {
+  var datasets = await fetchData("./data/datasets.json");
+
+  var datasetInfo = datasets.find((data) => data.name === selectedDataset);
+
+  const datasetSection = document.getElementById(`dataset-section`);
+  datasetSection.addEventListener('click', () => {
+    window.location.href = datasetInfo.url;
+  });
+  datasetSection.addEventListener('mouseover', () => {
+    tableRow.style.cursor = 'pointer';
+  });
+
+  const datasetTitle = document.getElementById(`dataset-name`);
+  datasetTitle.textContent = datasetInfo.name;
+
+  const datasetDescription = document.getElementById(`dataset-description`);
+  datasetDescription.textContent = datasetInfo.description;
+}
+
+
+getDatasetOptions();
+populateDatasetDescription();
+populateDatasetTable();
